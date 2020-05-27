b5c73fb07c60bff60b660888cd9da0a8ef45d6f4
Harry Borrelli <hborrelli@outlook.com>
2020-05-26 21:29:26 -0600

    Add tent icon and background image to landing screen.

--- src/Landing/Landing.tsx
+++ src/Landing/Landing.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { COLORS } from '../constants.js'
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ImageBackground } from 'react-native';
 import { useFonts } from '@use-expo/font';
 import { AppLoading } from 'expo';
 
@@ -25,24 +25,37 @@ export const Landing = ({ navigation }) => {
   } else {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>WilderNests</Text>
-        <View style={styles.button}>
-          <Button 
-            title="Find a Campsite"
-            onPress={() => handlePress()}
-            accessibilityLabel="Find a Campsite"
-            color='#fff'
-          />
-        </View>
-        <View style={styles.button}>
-          <Button
-            title="Post a Campsite"
-            onPress={() => handlePost()}
-            accessibilityLabel="Post a Campsite"
-            disabled={false}
-            color='#fff'
-          />
-        </View>
+        <ImageBackground
+          source={require('../../assets/images/landing-bg.png')}
+          style={styles.backgroundImage}
+        >
+          <View style={styles.logoBlock}>
+            <Image 
+              style={styles.icon}
+              source={require('../../assets/images/tent-icon.png')}
+            />
+            <Text style={styles.text}>WilderNests</Text>
+          </View>
+          <View style={styles.buttonBlock}>
+            <View style={styles.button}>
+              <Button 
+                title="Find a Campsite"
+                onPress={() => handlePress()}
+                accessibilityLabel="Find a Campsite"
+                color='#fff'
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title="Post a Campsite"
+                onPress={() => handlePost()}
+                accessibilityLabel="Post a Campsite"
+                disabled={false}
+                color='#fff'
+              />
+            </View>
+          </View>
+        </ImageBackground>
       </View>
     )
   }
@@ -52,7 +65,27 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: 'purple',
     flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
     padding: 20,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+  logoBlock: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonBlock: {
+    fontSize: 20,
+    flex: 2,
+  },
+  icon: {
+    width: 127,
+    height: 97,
+    marginBottom: 20,
+    marginTop: 100,
   },
   text: {
     fontSize: 30,
